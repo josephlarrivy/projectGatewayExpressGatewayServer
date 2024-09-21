af69715b7bb8ace116605dabe411dfb888ac2218
josephlarrivy <joseph.larrivy@gmail.com>
2024-09-21 18:31:14 -0500

    added cors and made a few more tweaks

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require("express");
 require('dotenv').config();
 const helmet = require("helmet");
+const cors = require("cors");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+
 
 const app = express();
 const requestLogger = require("./middleware/requestLogger");
 
+app.use(cors({
+    origin: process.env.LOCALHOST_ORIGIN
+}));
+
 app.use(express.json());
 app.use(requestLogger);
 app.use(helmet());
+
 
 
 // Proxy configuration to forward requests to Flask server
@@ -16,7 +23,7 @@ app.use('/data', createProxyMiddleware({
     target: process.env.FLASK_SERVER_TARGET,
     changeOrigin: true,
     pathRewrite: {
-        '^/data': '', // Remove '/flask' prefix
+        '^/data': '',
     },
 }));
 
@@ -25,7 +32,7 @@ app.use('/auth', createProxyMiddleware({
     target: process.env.DOTNET_SERVER_TARGET, 
     changeOrigin: true,
     pathRewrite: {
-        '^/auth': '', // Remove '/aspnet' prefix when forwarding
+        '^/auth': '', 
     },
 }));
 
