ab7e3095a01ba0bb1f415f9e3924ded59bb8fcfe
josephlarrivy <joseph.larrivy@gmail.com>
2024-09-20 20:02:09 -0500

    Getting server converted over to be a gateway

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 const express = require("express");
+const helmet = require("helmet");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
 const app = express();
-const testRoutes = require("./routes/testRoutes");
 const requestLogger = require("./middleware/requestLogger");
 
 app.use(express.json());
 app.use(requestLogger);
+app.use(helmet());
+
+// Proxy configuration to forward requests to Flask server
+app.use('/flask', createProxyMiddleware({
+    target: 'http://localhost:5001',
+    changeOrigin: true,
+    pathRewrite: {
+        '^/flask': '', // Remove '/flask' prefix
+    },
+}));
+
+// Proxy configuration to forward requests to ASP.NET Web API
+app.use('/aspnet', createProxyMiddleware({
+    target: 'http://localhost:5002', 
+    changeOrigin: true,
+    pathRewrite: {
+        '^/aspnet': '', // Remove '/aspnet' prefix when forwarding
+    },
+}));
 
 
-app.use(testRoutes);
+app.get('/test', (req, res) => {
+    res.send('Express API Gateway is running');
+});
 
 app.listen(3000, function () {
-    console.log("Server is listening on port 3000");
+    console.log("API Gateway is listening on port 3000");
 });
 
 module.exports = app;
--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = new express.Router();
 
-router.get('/test', (req, res, next) => {
+router.get('/testGet', (req, res, next) => {
     return res.send('good get test')
 });
 
-router.post('/test', (req, res, next) => {
+router.post('/testPost', (req, res, next) => {
     return res.send('good post test')
 });
 
